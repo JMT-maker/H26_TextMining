e6ea3f6de56ace3157670d0eb1fd6176141803fc
JMT-maker <jmtang0525@gmail.com>
2026-05-06 20:07:41 +0900

    Update WordFreqDashboard.py

--- TextMining/Streamlit/WordFreqDashboard.py
+++ TextMining/Streamlit/WordFreqDashboard.py
@@ -3,7 +3,7 @@ import mylib.myTextAnalyzer as ta
 import mylib.myStreamlitVisualizer as sv
 from konlpy.tag import Okt
 
-st.title("다음 영화 리뷰 빈도수 분석 대시보드")
+st.title("다음 영화 리뷰 빈도수 분석")
 datafile = r"D:\Lecture\TextMining_26\Apps\WordFreqProj\data\daum_movie_review.csv"
 
 # 1. 데이터 준비
@@ -13,8 +13,8 @@ corpus = ta.load_corpus(datafile, 'review')
 counter = ta.count_word_freq(corpus)
 
 # 3. 시각화 출력
-st.subheader("상위 키워드 막대 그래프")
+st.subheader("막대 그래프")
 sv.visualize_barh_grapyh(counter, 20)
 
-st.subheader("키워드 워드클라우드")
+st.subheader("워드클라우드")
 sv.visualize_wordcloud(counter, 50)
